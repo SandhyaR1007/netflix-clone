665cc75ceb3f17d1ce2dd2b41404f23ea1c9db9c
SandhyaR1007 <grajwanshi1008@gmail.com>
2022-10-27 23:52:26 +0530

    add: routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomeScreen from "./components/HomeScreen";
+import LoginScreen from "./components/LoginScreen";
 
 function App() {
+  const user = null;
   return (
     <div className="app">
-      <HomeScreen />
+      <BrowserRouter>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
+      </BrowserRouter>
     </div>
   );
 }
