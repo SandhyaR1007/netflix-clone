e37ab837e82d844732cb7915af69ef4d1c751f0b
SandhyaR1007 <grajwanshi1008@gmail.com>
2022-10-27 20:34:43 +0530

    add: Row component

--- src/components/Row.js
+++ src/components/Row.js
@@ -16,16 +16,20 @@ const Row = ({ title, fetchURL, isLargeRow = false }) => {
     <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
-        {movies.map((movie) => (
-          <img
-            key={movie?.id}
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            src={`${baseUrl}${
-              isLargeRow ? movie?.poster_path : movie?.backdrop_path
-            }`}
-            alt={movie.name}
-          />
-        ))}
+        {movies.map(
+          (movie) =>
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                key={movie?.id}
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`${baseUrl}${
+                  isLargeRow ? movie?.poster_path : movie?.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            )
+        )}
       </div>
     </div>
   );
