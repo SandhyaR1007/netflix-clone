84db32d0077ab2a7a555b4fd1eb79db755f33693
SandhyaR1007 <grajwanshi1008@gmail.com>
2022-10-28 23:02:14 +0530

    update: styles for signup form

--- src/components/SignupScreen.js
+++ src/components/SignupScreen.js
@@ -9,6 +9,10 @@ const SignupScreen = () => {
         <input placeholder="Email Address" type="email" />
         <input placeholder="Password" type="password" />
         <button type="submit">Sign In</button>
+        <p>
+          <span className="signupScreen__gray">New to Netflix? </span>
+          <span className="signupScreen__link">Sign up now.</span>
+        </p>
       </form>
     </div>
   );
