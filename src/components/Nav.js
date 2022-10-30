f22e2d183f4063a0b32f13c7adfdff6837ba1518
SandhyaR1007 <grajwanshi1008@gmail.com>
2022-10-30 20:39:13 +0530

    add: routing to profile screen and home screen

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 import "./styles/Nav.css";
 const Nav = () => {
   const [showNav, setShowNav] = useState(false);
+  const navigate = useNavigate();
   const transitionNavbar = () => {
     if (window.scrollY > 90) {
       setShowNav(true);
@@ -22,11 +24,17 @@ const Nav = () => {
           //src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           src="https://pngimg.com/uploads/netflix/netflix_PNG25.png"
           alt="netflix-logo"
+          onClick={() => {
+            navigate("/");
+          }}
         />
         <img
           className="nav__avatar"
           src="https://i.pinimg.com/originals/61/54/76/61547625e01d8daf941aae3ffb37f653.png"
           alt="avatar"
+          onClick={() => {
+            navigate("/profile");
+          }}
         />
       </div>
     </div>
