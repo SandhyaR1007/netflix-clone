b28a80976c2bf751e7b2fc303bd1cf5b21be7df4
SandhyaR1007 <grajwanshi1008@gmail.com>
2022-10-30 20:38:32 +0530

    update: login, logout based on user info from auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import HomeScreen from "./components/HomeScreen";
 import LoginScreen from "./components/LoginScreen";
 import { auth } from "./firebase";
+import { login, logout, selectUser } from "./features/userSlice";
+import ProfileScreen from "./components/ProfileScreen";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
         //logged in
         console.log({ userAuth });
+        dispatch(
+          login({
+            uid: userAuth?.uid,
+            email: userAuth?.email,
+          })
+        );
       } else {
         //logged out
+        dispatch(logout());
       }
     });
 
     return unsubscribe;
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="app">
@@ -27,6 +38,7 @@ function App() {
           <LoginScreen />
         ) : (
           <Routes>
+            <Route path="/profile" element={<ProfileScreen />} />
             <Route path="/" element={<HomeScreen />} />
           </Routes>
         )}
